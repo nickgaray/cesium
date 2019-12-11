aaca4e22f932411e8c7888cbff68ee21044f0d44
Nicolas Garay <nicolasgaray@icloud.com>
2019-12-11 14:52:17 -0600

    Modify Image Draping Shader - Draw pixels from source canvas

--- Source/Shaders/ImageDrapingFS.glsl
+++ Source/Shaders/ImageDrapingFS.glsl
@@ -82,5 +82,5 @@ void main()
 
     //float depth = pow(v_posEC.z * 0.5 + 0.5, 8.0);
     //gl_FragColor = vec4(depth, depth, depth, 1.0);
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+//    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
